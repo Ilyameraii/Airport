b57a398f809c95f7484b2979029c1595490455fe
Ilya Bobrov <ilyaraibusiness@gmail.com>
2025-10-14 19:03:37 +0300

    Создал интерфейс для сервиса отчетности. Создал сервис отчетности на основе этого интерфейса.

---
+++ Airport/Interfaces/IReportInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport.Interfaces
+{
+    internal interface IReportInfo
+    {
+        int TotalPassangers { get; }
+
+        int TotalArrivingFlights { get; }
+
+        int TotalCrew { get; }
+
+        decimal TotalRevenue { get; }
+    }
+}
---
+++ Airport/Services/ReportingService.cs
@@ -0,0 +1,48 @@
+﻿using Airport.Interfaces;
+using Airport.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport.Services
+{
+    internal class ReportingService : IReportInfo
+    {
+        private IEnumerable<Flight> flights;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="flights">Рейсы</param>
+        public ReportingService(IEnumerable<Flight> flights)
+        {
+            this.flights = flights;
+        }
+
+        /// <summary>
+        /// Сумма всех пассажиров
+        /// </summary>
+        public int TotalPassangers => flights.Sum(f => f.NumberOfPassengers);
+
+        /// <summary>
+        /// Сумма всех экипажей
+        /// </summary>
+        public int TotalCrew=>flights.Sum(f => f.NumberOfCrew);
+
+        /// <summary>
+        /// Сумма всех рейсов
+        /// </summary>
+        public int TotalArrivingFlights => flights.Count();
+
+        /// <summary>
+        /// Суммарная выручка
+        /// </summary>
+        public decimal TotalRevenue => flights.Sum(f => f.Revenue);
+
+
+
+
+    }
+}
